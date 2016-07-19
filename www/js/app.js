21319e6330eeb85b9982bd9fea1c940d41967252
Ajit <jainajit194@gmail.com>
2016-07-19 11:42:42 +0530

    chk

--- www/js/app.js
+++ www/js/app.js
@@ -52,5 +52,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         
       })
       ;
-      $urlRouterProvider.otherwise('/SplashScreen');
+      $urlRouterProvider.otherwise('/initial');
 });
