d38984896043381544880b24318b7764b0a8a614
Ajit <jainajit194@gmail.com>
2016-07-28 16:53:04 +0530

    ddjd

--- www/js/app.js
+++ www/js/app.js
@@ -125,7 +125,7 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.loginControll
         templateUrl:'templates/terms.html'
       })
       ;
-      $urlRouterProvider.otherwise('/home/match');
+      $urlRouterProvider.otherwise('/login');
 
 
 })
